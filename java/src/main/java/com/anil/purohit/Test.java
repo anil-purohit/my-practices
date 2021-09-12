b9465d0fae1150ec589ecc20537621d0f601a44e
Anil Purohit <anil.purohit@flipkart.com>
2021-09-12 09:46:28 +0530

    Added hello world file

---
+++ java/src/main/java/com/anil/purohit/Test.java
@@ -0,0 +1,11 @@
+package com.anil.purohit;
+
+/**
+ * @author anil.purohit
+ * @since 12/09/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
