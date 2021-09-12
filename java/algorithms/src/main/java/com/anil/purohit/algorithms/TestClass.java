dc5031feae986310877601573c73b2979f1fd52c
anil-purohit <anilpurohit711@gmail.com>
2021-09-12 10:06:01 +0530

    test class

---
+++ java/algorithms/src/main/java/com/anil/purohit/algorithms/TestClass.java
@@ -0,0 +1,11 @@
+package com.anil.purohit.algorithms;
+
+/**
+ * @author anil.purohit
+ * @since 12/09/21.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
